643395d42b4d9b23ca50d65fef1f67e93d7e9e39
Aleksandr Chupakhin <79205937997@ya.ru>
2018-05-18 16:05:21 +0300

    Plain text renderer and tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,34 +1,35 @@
 import fs from 'fs';
 import path from 'path';
 import genDiff from '../src';
+import { renderJsonLike, renderPlainText } from '../src/renderers';
 
 const buildFixturePath = fileName =>
   path.join(__dirname, '__fixtures__', fileName);
 
 describe('genDiff', () => {
-  describe('valid formats', () => {
+  describe('valid input formats', () => {
     let expectedDiff;
 
     beforeAll(() => {
-      const expectedDiffPath = buildFixturePath('expected-diff');
+      const expectedDiffPath = buildFixturePath('expected-json-like');
       expectedDiff = fs.readFileSync(expectedDiffPath, 'utf-8').trim();
     });
 
-    it('JSON format', () => {
+    it('json format', () => {
       const fixtures = ['before.json', 'after.json'];
       const [beforePath, afterPath] = fixtures.map(buildFixturePath);
 
       expect(genDiff(beforePath, afterPath)).toEqual(expectedDiff);
     });
 
-    it('YAML format', () => {
+    it('yaml format', () => {
       const fixtures = ['before.yml', 'after.yml'];
       const [beforePath, afterPath] = fixtures.map(buildFixturePath);
 
       expect(genDiff(beforePath, afterPath)).toEqual(expectedDiff);
     });
 
-    it('INI format', () => {
+    it('ini format', () => {
       const fixtures = ['before.ini', 'after.ini'];
       const [beforePath, afterPath] = fixtures.map(buildFixturePath);
 
@@ -36,7 +37,30 @@ describe('genDiff', () => {
     });
   });
 
-  it('unknown format throws error', () => {
+  describe('output renderers', () => {
+    let beforePath;
+    let afterPath;
+    beforeAll(() => {
+      const fixtures = ['before.json', 'after.json'];
+      [beforePath, afterPath] = fixtures.map(buildFixturePath);
+    });
+
+    it('json-like renderer', () => {
+      const expectedDiffPath = buildFixturePath('expected-json-like');
+      const expectedDiff = fs.readFileSync(expectedDiffPath, 'utf-8').trim();
+
+      expect(genDiff(beforePath, afterPath, renderJsonLike)).toEqual(expectedDiff);
+    });
+
+    it('plain-text renderer', () => {
+      const expectedDiffPath = buildFixturePath('expected-plain-text');
+      const expectedDiff = fs.readFileSync(expectedDiffPath, 'utf-8').trim();
+
+      expect(genDiff(beforePath, afterPath, renderPlainText)).toEqual(expectedDiff);
+    });
+  });
+
+  it('unknown input format throws error', () => {
     const fixtures = ['before.unknown', 'after.yml'];
     const [beforePath, afterPath] = fixtures.map(buildFixturePath);
 
--- src/renderers/plain-text.js
+++ src/renderers/plain-text.js
@@ -1,3 +1,38 @@
-const render = () => '###';
+import _ from 'lodash';
+
+const buildIntro = (key, parentPath) =>
+  `Property '${parentPath}${key}' was`;
+
+const stringifyValue = (value) => {
+  if (_.isObject(value)) {
+    return 'complex value';
+  }
+  if (_.isString(value)) {
+    return `'${value}'`;
+  }
+  return value;
+};
+
+const mapping = {
+  unchanged: () => '',
+  added: ({ key, value }, parentPath) =>
+    `${buildIntro(key, parentPath)} added with value: ${stringifyValue(value)}`,
+  removed: ({ key }, parentPath) =>
+    `${buildIntro(key, parentPath)} removed`,
+  changed: ({ key, oldValue, newValue }, parentPath) =>
+    `${buildIntro(key, parentPath)} updated. From ${stringifyValue(oldValue)} to ${stringifyValue(newValue)}`,
+  nested: ({ key, children }, parentPath, stringifyAst) =>
+    stringifyAst(children, `${parentPath}${key}.`),
+};
+
+const render = (ast) => {
+  const stringifyAst = (tree, parentPath) => {
+    const strings = tree
+      .map(node => mapping[node.type](node, parentPath, stringifyAst))
+      .filter(el => el !== '');
+    return _.flatten(strings);
+  };
+  return stringifyAst(ast, '').join('\n');
+};
 
 export default render;
